f31ede998c937ac983ab9875b214d2199b36ed46
kaduce2009@hotmail.com <kaduce2009@hotmail.com>
2020-05-18 15:46:06 -0300

    Primeira vez mexendo

---
+++ app/src/main/java/com/example/testeabc/Add_Activity.java
@@ -0,0 +1,14 @@
+package com.example.testeabc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_layout);
+    }
+}
